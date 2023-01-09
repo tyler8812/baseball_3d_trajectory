41b9901e9785d38f1fc1612047fb74db72492c98
tyler8812 <tyler88128949@gmail.com>
2023-01-09 11:46:12 +0800

    feat: add main.ps1 for create input path, predict, convert yolov7 result

--- main.ps1
+++ main.ps1
@@ -4,29 +4,54 @@ cd ${FOLDERPATH}
 echo "Entering ${FOLDERPATH}......"
 ./env/Scripts/activate
 
-$VIEW1VIDEO='2022-11-22-6-1.MP4'
-$VIEW2VIDEO='2022-11-22-6-2.MP4'
+# single video
+$VIEW1VIDEO="2022-12-19-5-1.MP4"
+$VIEW2VIDEO="2022-12-19-5-2.MP4"
 $VIEW1NAME=$VIEW1VIDEO.Split('.')[0]
 $VIEW2NAME=$VIEW2VIDEO.Split('.')[0]
 
+mkdir input_file
+mkdir output
+mkdir "input_file/$VIEW1NAME"
+mkdir "input_file/$VIEW2NAME"
+
+Copy "video/${VIEW1VIDEO}" "input_file/$VIEW1NAME"
+Copy "video/${VIEW2VIDEO}" "input_file/$VIEW2NAME"
 
 echo "Detecting ball from ${VIEW1VIDEO}......"
 python yolov7/detect.py `
---weights yolov7/weights/yolov7_baseball.pt `
---conf 0.4 `
+--weights yolov7/weights/yolov7-w6_baseball.pt `
+--conf 0.3 `
 --img-size 1920 `
 --source video/${VIEW1VIDEO} `
 --name $VIEW1NAME `
 --view-img `
---save-txt
+--save-txt `
+--save-conf
 
 echo "Detecting ball from ${VIEW2VIDEO}......"
 python yolov7/detect.py `
---weights yolov7/weights/yolov7_baseball.pt `
---conf 0.4 `
+--weights yolov7/weights/yolov7-w6_baseball.pt `
+--conf 0.3 `
 --img-size 1920 `
 --source video/${VIEW2VIDEO} `
 --name $VIEW2NAME `
 --view-img `
---save-txt
+--save-txt `
+--save-conf
 
+echo "Convert yolov7 result from ${VIEW1VIDEO}....."
+python utils/convert_yolov7_result.py `
+--input_folder "./runs/detect/${VIEW1NAME}/labels" `
+--output "./input_file/${VIEW1NAME}/labels.npy" `
+--input_video "./runs/detect/${VIEW1NAME}/${VIEW1VIDEO}" `
+--video_name $VIEW1NAME `
+--view 1
+
+echo "Convert yolov7 result from ${VIEW2VIDEO}......"
+python utils/convert_yolov7_result.py `
+--input_folder "./runs/detect/${VIEW2NAME}/labels" `
+--output "./input_file/${VIEW2NAME}/labels.npy" `
+--input_video "./runs/detect/${VIEW2NAME}/${VIEW2VIDEO}" `
+--video_name $VIEW2NAME `
+--view 2
